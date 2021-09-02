a16630d15ce913314ddc399efc3eee5d41190ee2
Fatima zahra Sarbout <fsarbout@e2r9p12.1337.ma>
2021-09-02 16:50:53 +0100

    _displayTimestamp

--- modue00/ex02/Account.cpp
+++ modue00/ex02/Account.cpp
@@ -1,2 +1,80 @@
 #include "Account.hpp"
+#include <iostream>
 
+Account::Account(int initial_deposit)
+{
+	initial_deposit = 0;	
+}
+
+Account::~Account( void )
+{
+	return;
+}
+
+int	Account::getNbAccounts( void )
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount( void )
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits( void )
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals( void )
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos( void )
+{
+	_displayTimestamp();
+	std::cout << "accounts:" << getNbAccounts() << ";";
+	std::cout << "total" << getTotalAmount() << ";";
+	std::cout << "deposits" << getNbDeposits() << ";";
+	std::cout << "withdrawals" << getNbWithdrawals() << ";";
+	std::cout << std::endl;
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	 deposit = 0;
+	return;
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	withdrawal = 0;
+	return false;
+}
+
+int		Account::checkAmount(void) const
+{
+	return 0;
+}
+
+void	Account::displayStatus(void) const
+{
+	_displayTimestamp();
+	return;
+}
+
+
+void	Account::_displayTimestamp(void)
+{
+	std::time_t			now;
+	std::tm				*localtime;
+	char			date[80];
+	std::string			format;
+
+	now = std::time(NULL);
+	localtime = std::localtime(&now);
+	format = "[%Y%m%d_%H%M%S] ";
+	std::strftime(date, 80, format.c_str(), localtime);
+	std::cout << date;
+}

3a8e65e5a1ea77ad56a814abed890d29430d5bd2
fsarbout <fatimazahrasarbout@gmail.com>
2021-09-04 10:06:19 +0100

    Done

--- modue00/ex00/megaphone.cpp
+++ modue00/ex00/megaphone.cpp
@@ -6,27 +6,25 @@
 /*   By: fsarbout <fsarbout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 20:16:18 by fsarbout          #+#    #+#             */
-/*   Updated: 2021/07/15 19:25:20 by fsarbout         ###   ########.fr       */
+/*   Updated: 2021/09/04 10:04:00 by fsarbout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-
-/* for linux */
 #include <string.h>
+
+#define string std::string
+#define cout std::cout
+#define endl std::endl
 
 int main(int ac, char **ag)
 {
-	char str[] = "* loud and unbrearable feedback noise *\n";
 	if (ac == 1)
-	{
-		for (size_t i = 0; i < strlen(str); i++)
-			putchar(toupper(str[i]));
-	}
+		cout << "* LOUD AND UNBREARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; ++i )
 	{
 		for(size_t j = 0; j < strlen(ag[i]); j++)
-			putchar(toupper(ag[i][j]));
-		std::cout << "\n";
+			putwchar(toupper(ag[i][j]));
 	}
+	cout << endl;
 }

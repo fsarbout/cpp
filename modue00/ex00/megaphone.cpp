0a94151dcf672bfa62e31d043f2c6e4e43df15cd
fsarbout <fatimazahrasarbout@gmail.com>
2021-09-05 18:30:23 +0100

    edit wrong msg, done

--- modue00/ex00/megaphone.cpp
+++ modue00/ex00/megaphone.cpp
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   metaphone.cpp                                      :+:      :+:    :+:   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fsarbout <fsarbout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/14 20:16:18 by fsarbout          #+#    #+#             */
-/*   Updated: 2021/09/04 10:04:00 by fsarbout         ###   ########.fr       */
+/*   Updated: 2021/09/05 16:13:22 by fsarbout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,14 @@
 int main(int ac, char **ag)
 {
 	if (ac == 1)
-		cout << "* LOUD AND UNBREARABLE FEEDBACK NOISE *";
+		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	for (int i = 1; i < ac; ++i )
 	{
 		for(size_t j = 0; j < strlen(ag[i]); j++)
-			putwchar(toupper(ag[i][j]));
+			cout << (toupper(ag[i][j]));
+			// putwchar(toupper(ag[i][j]));
+			cout << putwchar(7272);
+		// toupper 
 	}
 	cout << endl;
 }

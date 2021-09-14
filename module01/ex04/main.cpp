d7f2caddac6883a42ee0d43660d4c7acea9e63f5
fsarbout <fatimazahrasarbout@gmail.com>
2021-09-14 10:01:50 +0100

    .

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 #include <fstream>
 
+#define GREEN "\e[1;32m"
+#define RED "\e[1;31m"
+#define WHITE "\e[1;37m"
+#define YELLOW "\e[1;33m"
+#define BLUE "\e[1;34m"
+#define PURPLE "\033[1;35m"
+
 #define cout std::cout
 #define cin std::cin
 #define string std::string
@@ -9,12 +16,6 @@
 #define ifstream std::ifstream
 #define getline(a, s) std::getline(a , s)
 
-#define GREEN "\e[1;32m"
-#define RED "\e[1;31m"
-#define WHITE "\e[1;37m"
-#define YELLOW "\e[1;33m"
-#define BLUE "\e[1;34m"
-#define PURPLE "\033[1;35m"
 
 
 int main(int ac, char **av)
@@ -31,28 +32,21 @@ int main(int ac, char **av)
 		cout << "iFileName : " << iFileName << endl; 
 		cout << "oFileName : " << oFileName << endl; 
 
-		// string s4
-		// ofstream file(av[3]);
-		// ifstream file((av[3] + ".replace"));
-
 		if (!oFile.is_open())
 			cout << "fuck you\n";
 		else
 			cout << "thank you\n";
-		// else
-		// {
-		// 	writeCreateFile << "ma fucksh you\n" << "it's actually working \n" << endl << "" << endl << "hola amigos\n";
-		// 	writeCreateFile.close();
-		// }
-		// string toRread;
-		// if (readFile.is_open())
-		// {
-		// 	while (getline(readFile, toRread))
-		// 	{
-		// 		cout << toRread << endl;
-		// 	}
-		// 	readFile.close();
+
+		string toRread;
+		if (oFileName.is_open())
+		{
+			while (getline(oFileName, toRread))
+			{
+				cout << toRread << endl;
+			}
+			oFileName.close();
 		}
+	}
 	else
 		cout << "\e[1;31m number of arguments less or more than 4 \e[0;37m" << endl ;
 }

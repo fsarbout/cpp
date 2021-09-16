1b06373820bdff024cdc179c4939ef93c618329b
fsarbout <fatimazahrasarbout@gmail.com>
2021-09-16 18:05:25 +0100

    .

--- module01/ex04/main.cpp
+++ module01/ex04/main.cpp
@@ -3,10 +3,7 @@
 
 #define GREEN "\e[1;32m"
 #define RED "\e[1;31m"
-#define WHITE "\e[1;37m"
-#define YELLOW "\e[1;33m"
-#define BLUE "\e[1;34m"
-#define PURPLE "\033[1;35m"
+#define DEFAULT "\e[0;37m"
 
 #define cout std::cout
 #define cin std::cin
@@ -16,58 +13,60 @@
 #define ifstream std::ifstream
 #define getline(a, s) std::getline(a , s)
 
-
-// int     errorHandling(string s1, string s2, )
-
+int error(ifstream &iFile, string s1 , string s2)
+{
+    if (!iFile.is_open())
+    {
+        cout << "\e[1;31m CAN'T OPEN FILE \e[0;37m" << endl ;  
+        return (1);
+    } 
+    if (iFile.peek() ==  EOF)
+    {
+        cout << "\e[1;31m FILE IS EMPTY \e[0;37m" << endl ;
+        return (1);
+    }
+    if (s1.compare("") == 0 || s2.compare("") == 0)
+    {
+        cout << "\e[1;31m EMPTY STRING \e[0;37m" << endl ;
+        return (1);
+    }
+    return (0);
+}
 
 int main(int ac, char **av)
 {
-
     if (ac == 4)
     {
         string iFileName = av[1];
         string oFileName = iFileName + ".replace";
 
-        /*ofstream == create and write information to file */
+        /* ofstream == create and write information to file */
         ofstream oFile(oFileName);
-        /*ifstream == read information from file */
+        /* ifstream == read information from file */
         ifstream iFile(iFileName);
 
         string s1 = av[2];
         string s2 = av[3];
 
-        if (!iFile.is_open())
+        if (error(iFile,  s1 ,  s2))
+            return (1);
+        
+        string line;
+        size_t pos = 0;
+        while (getline(iFile, line))
         {
-            cout << "\e[1;31m CAN'T OPEN FILE \e[0;37m" << endl ;  
-            return 0;
-        } 
-        if (s1.compare("") == 0 || s2.compare("") == 0)
-        {
-            cout << "\e[1;31m EMPTY STRING \e[0;37m" << endl ;  
-            return 0;
-        }
-        else
-        {
-            int count = 0;
-            stringPos;
-            string line;
-            while (getline(iFile, line))
+            while ((pos = line.find(s1, pos)) != string::npos)
             {
-                if (s1.find(line) != 0)
-                {
-                    cout << endl;
-                    cout << "I find the world \n";
-                    cout << line << endl;
-                    count++;
-                }
-                cout << count << "times\n";
-                
+                line.erase(pos, s1.length());
+                line.insert(pos, s2);
+                pos = pos + s1.length();
             }
-
-            cout << "s1 : " << s1 << endl;
-            cout << "s2 : " << s2 << endl;
+            oFile << line << endl;
         }
+        iFile.close();
+        oFile.close();
+        return (0);
     }
-    else 
-		cout << "\e[1;31m number of arguments less or more than 4 \e[0;37m" << endl ;       
+    cout << "\e[1;31m NUMBER OF ARGUMENTS LESS OR MORE THAN 4 \e[0;37m" << endl;
+    return (1);
 }

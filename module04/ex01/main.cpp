e1097bb9633a13b9510ec3dab324929bdefa60e9
fsarbout <fatimazahrasarbout@gmail.com>
2021-10-15 12:16:34 +0100

    .

--- module04/ex01/main.cpp
+++ module04/ex01/main.cpp
@@ -5,15 +5,20 @@
 
 int main()
 {
-	const Animal* animal[100];
+	const Animal* animal[2];
 
 
-	for (int k = 0; k < 10; k++)
-		animal[k] = new Dog();
-	for (int k = 10; k < 20; k++)
-		animal[k] = new Dog();
-	for (int i = 0; i < 20; i++)
-		delete animal[i];
+	animal[0] = new Dog();
+	animal[1] = new Dog();
+
+	for (int i = 0; i < 2; i++)
+		delete [] animal;
+
+	// Dog dog;
+	// Cat cat;
+
+	
 }
+
 
 

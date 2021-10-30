bb489d37d696614e582e64adc36f70c133dcddcc
fsarbout <fatimazahrasarbout@gmail.com>
2021-10-30 20:25:20 +0100

    .

--- module05/ex03/main.cpp
+++ module05/ex03/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	Bureaucrat b1("b1", 1);
+	Bureaucrat b1("b1", 120);
 	Bureaucrat b2("b2", 150);
 
 	Intern intern = Intern();
@@ -10,6 +10,42 @@ int main()
 	Form *shForm = intern.makeForm("shForm", "target");
 	Form *rrForm = intern.makeForm("rrForm", "target1");
 	Form *ppForm = intern.makeForm("ppForm", "target2");
+
+	cout << "*--------------------------shForm---------------------------*" << endl;
+
+	try
+	{
+		b1.signForm(*shForm);
+		b1.executeForm(*shForm);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << endl;
+	}
+
+	cout << "*--------------------------rrForm---------------------------*" << endl;
+
+	try
+	{
+		b1.signForm(*rrForm);
+		b1.executeForm(*rrForm);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << endl;
+	}
+
+	cout << "*--------------------------ppForm---------------------------*" << endl;
+
+	try
+	{
+		b1.signForm(*ppForm);
+		b1.executeForm(*ppForm);
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << endl;
+	}
 
 	delete shForm;
 	delete rrForm;

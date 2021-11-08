ae2a8137874e952819cb2fd79da9baaa9eb82119
fsarbout <fatimazahrasarbout@gmail.com>
2021-11-08 13:27:31 +0100

    init

---
+++ module06/ex00/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#define cout std::cout
+#define endl std::endl
+#define string std::string
+
+int main(int ac, char **av)
+{
+	if (ac == 2)
+	{
+		cout << av[1] << endl;
+		// cast();
+		isValidType();
+		isDisplayable();
+	}
+}
+
+
+
+
+
+
+
+

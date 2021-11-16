bcd77a263b90c7a4755b63a3eb7ae03727d57909
fsarbout <fatimazahrasarbout@gmail.com>
2021-11-16 16:50:06 +0100

    .

--- module07/ex00/functions.cpp
+++ module07/ex00/functions.cpp
@@ -3,7 +3,6 @@
 /*************** MIN *****************/	
 
 template <typename T>
-
 T min(T a, T b)
 {
 	return (a < b)? a: b;
@@ -12,7 +11,6 @@ T min(T a, T b)
 /*************** MAX *****************/	
 
 template <typename T>
-
 T max(T a, T b)
 {
 	return (a > b)? a: b;
@@ -21,10 +19,21 @@ T max(T a, T b)
 /*************** SWAP ****************/	
 
 template <typename T>
-
 void swap(T a, T b)
 {	
 	T c = a;
 	a = b;
 	b = c;
 }
+
+// parametric macros 
+// in template compiler write code for us 
+
+// the template is evaluated at compile time
+// it not a code or a function , it is only created when we call it , and based on how we call it
+// it's just a blueprint (schema) of the code that we want to create
+
+// if we made an error in the template and don't called it ,  
+// it won't show any error in compile time (cuz this code isn't really exist). >> depends on the compiler : clang++ will show it
+
+// they are very useful but if an error occurs you have to spend half your life to figure out which code was compiled and showed this error

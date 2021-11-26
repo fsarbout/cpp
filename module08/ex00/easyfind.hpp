437a7de53c57a96c990ce49421f40c0e5266f2f9
fsarbout <fatimazahrasarbout@gmail.com>
2021-11-26 01:26:13 +0100

    start day08

---
+++ module08/ex00/easyfind.hpp
@@ -0,0 +1,13 @@
+#ifndef EASYFING_HPP
+#define EASYFING_HPP
+
+template <typename T>
+
+// std::list<int>   t2
+// std::map         map1
+// std::vector
+
+// t2.push_back(1);
+// t2.push_back(14513);
+
+#endif
